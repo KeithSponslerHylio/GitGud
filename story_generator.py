b74bd3ea8d16c1ec38566038d3b1d93387f3f20b
KeithSponslerHylio <109163945+KeithSponslerHylio@users.noreply.github.com>
2026-05-12 15:31:06 -0500

    initial commit

---
+++ story_generator.py
@@ -0,0 +1,32 @@
+import re
+
+# Select which story to publish (Comment out the unused stories)
+story_genre = 'fantasy'
+# story_genre = 'scifi
+
+# Ingest nouns into a list
+with open('words/nouns.txt', 'r') as f:
+    nouns = f.read().splitlines()
+
+# Ingest verbs into a list
+with open('words/verbs.txt', 'r') as f:
+    verbs = f.read().splitlines()
+
+# Ingest the story template
+with open('stories/' + story_genre + '.txt', 'r') as f:
+    story_template = f.read()
+
+# This function will replace the (n1) and (v2)'s with real words
+def replace_placeholder(match):
+    word_type = match.group(1)
+    index = int(match.group(2)) - 1
+    
+    if word_type == 'n':
+        return nouns[index]
+    elif word_type == 'v':
+        return verbs[index]
+
+completed_story = re.sub(r'\((n|v)(\d+)\)', replace_placeholder, story_template)
+
+with open('stories/PUBLISH_THIS.txt', 'w') as f:
+    f.write(completed_story)
